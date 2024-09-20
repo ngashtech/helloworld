bcd61072f939153e7f003159874e271f432ac1b2
root <root@localhost>
2024-09-20 16:05:56 +0300

    auto publish 1726837555385

---
+++ src/main.c
@@ -0,0 +1,8 @@
+//Francis ngashtech project 
+
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    printf("Hello world\n"); printf("ngashtechfrancis\n"); printf("i love you guys you are doing a good job out there\n");printf("congratulations\n"); printf("pa107francis\n");
+    printf("942052\n"); printf("Good morning guys GOD loves us \n"); printf("Jesus Christ died for us on the cross so that we may receive salvation\n "); printf("read your Bible and pray every day ");
+return 0;}
